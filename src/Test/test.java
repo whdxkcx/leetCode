fd19f4dc5c76c775e966b2ec4ab88a9855bb37a8
whdxkcx <862557688@qq.com>
2019-05-10 13:25:06 +0800

    2019.5.8

--- src/Test/test.java
+++ src/Test/test.java
@@ -5,19 +5,21 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args)  {
-        Scanner in=new Scanner(System.in);
-        int n=in.nextInt();
-        int maxsize = 1 << n;
-
-        for(int i = 0; i < maxsize; ++i)
-        {
-            int t=i ^ (i >> 1);
-            for(int j = n; j > 0; --j) {
-                int temp=t>>j-1;
-                System.out.print(temp&1);
-            }
-            System.out.println();
-        }
-        return ;
+//        Scanner in=new Scanner(System.in);
+//        int n=in.nextInt();
+//        int maxsize = 1 << n;
+//
+//        for(int i = 0; i < maxsize; ++i)
+//        {
+//            int t=i ^ (i >> 1);
+//            for(int j = n; j > 0; --j) {
+//                int temp=t>>j-1;
+//                System.out.print(temp&1);
+//            }
+//            System.out.println();
+//        }
+//        return ;
+        String t="t,,t";
+        System.out.println(t.split(",").length);
     }
 }
